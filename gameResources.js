29864f387dc4937c9f556a134b057d332d28f81d
spacemars <mroushey@gmail.com>
2012-04-21 17:29:15 -0400

    bg tiles and new Tiled map

--- gameResources.js
+++ gameResources.js
@@ -11,5 +11,7 @@ var gameResources =
     { name: "player", type: "image", src: "data/player.png" },
     { name: "dude01", type: "image", src: "data/dude01.png" },
     { name: "area01_level_tiles", type: "image", src: "data/area01_level_tiles.png" },
+    { name: "collision_tiles", type: "image", src: "data/collision_tiles.png" },
+	{ name: "area01_parallax1", type: "image", src: "data/area01_parallax1.png" },
     { name: "level0", type: "tmx", src: "data/level0.tmx" }
 ];
